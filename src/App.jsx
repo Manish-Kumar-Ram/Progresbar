00d281fe46ac35dad9a497e9c775a1fe64ff37de
Manish Kumar Ram <manishkaran9809@gmail.com>
2024-06-14 20:43:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import './App.css'
+import Progresbar from './Progresbar'
+
+
+function App() {
+const [count,setcount] = useState(0)
+
+useEffect(()=>{
+  const time= setInterval(()=>{
+    if(count<100)
+      {
+        setcount(count+1)
+      }
+
+  },100)
+
+  return()=>{
+    clearInterval(time)
+  }
+  
+},[count])
+
+
+  return (
+    <div className='app'>
+      <Progresbar
+      progress = {count}
+      color = {'lightgreen'}/>
+    </div>
+
+
+  )
+}
+
+export default App
